7be1a8e05e140b3ea6885a15c7b8e800d2e4f6b6
Sagar Avale <sagar9510@gmail.com>
2019-12-30 14:15:11 +0000

    commit2

--- Audio.py
+++ Audio.py
@@ -14,15 +14,15 @@ def audio_2_array(path):
     a = read(path)
     return np.array(a[1],dtype=float)
 
-def array_2_audio(audio_array,samplerate= 44100):
+def array_2_audio(audio_array,filename = 'test',samplerate= 44100):
+    a_file = str('./data/' + filename + '.wav')
     scaled = np.int16(audio_array/np.max(np.abs(audio_array)) * 32767)
-    write('./data/test.wav', samplerate, scaled)
+    write(a_file, samplerate, scaled)
     return 0
 
 song = audio_2_array(song_path)
+array_2_audio(song)
 
 #Reverse song -
-
 rev_song = np.flip(song)
-
-array_2_audio(rev_song,22100)
+array_2_audio(rev_song,filename='rev_audio')
